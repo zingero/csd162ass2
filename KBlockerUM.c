4c475af845927c271912c2af84791d4edc411e1f
oshrat <oshrat.bar@gmail.com>
2016-06-06 10:44:58 -0700

    sha for .txt &  ELF

--- KBlockerUM.c
+++ KBlockerUM.c
@@ -7,6 +7,8 @@ Source: http://stackoverflow.com/questions/3299386/how-to-use-netlink-socket-to-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <openssl/sha.h>
+#include <sys/stat.h>
 
 #define NETLINK_USER 31
 
@@ -18,25 +20,83 @@ int sock_fd;
 struct msghdr msg;
 char message[MAX_PAYLOAD] = {0};
 
+char* file_content(FILE* file, int size){
+    int  c, i = 0;
+    char* buffer = malloc(size);
+    
+    while((c = fgetc(file)) != EOF){
+        buffer[i] = c;
+        i++;
+    }
+    return buffer;
+}
+
+
+
+
+
+
+void calculate_sha256(char *string, char outputBuffer[65], int size){
+    unsigned char hash[32];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, string, size);
+    SHA256_Final(hash, &sha256);
+    int i = 0;
+    for(i = 0; i < 32; i++)
+    {
+        sprintf(outputBuffer + (i * 2), "%02x", (unsigned char)hash[i]);
+    }
+
+    outputBuffer[64] = 0;
+}
+
+void sha_file(){
+   FILE *fp;
+   char *str;
+   fp = fopen(NLMSG_DATA(nlh),"r");
+   if(fp == NULL) 
+   {
+      printf("Error in opening file\n");
+     return;
+   }
+   
+   //calculate buffer's size
+    struct stat st;
+    stat(NLMSG_DATA(nlh), &st);
+    int size = st.st_size;
+
+    str = file_content(fp, size);
+    fclose(fp);
+
+    //call sha
+    static unsigned char buffer[65];
+    calculate_sha256(str, buffer, size);
+  //  printf("%s\n", buffer);
+    strcpy(NLMSG_DATA(nlh), buffer);
+    
+}
 int main()
 {
+   
+
     sock_fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_USER);
     if (sock_fd < 0)
     {
-	    printf("ERROR in user program!!!\n");
-	    return -1;
+        printf("ERROR in user program!!!\n");
+        return -1;
     }
 
     memset(&src_addr, 0, sizeof(src_addr));
     src_addr.nl_family = AF_NETLINK;
-    src_addr.nl_pid = getpid(); /* self pid */
+    src_addr.nl_pid = getpid(); // self pid 
     bind(sock_fd, (struct sockaddr *)&src_addr, sizeof(src_addr));
 
     memset(&dest_addr, 0, sizeof(dest_addr));
     memset(&dest_addr, 0, sizeof(dest_addr));
     dest_addr.nl_family = AF_NETLINK;
-    dest_addr.nl_pid = 0; /* For Linux Kernel */
-    dest_addr.nl_groups = 0; /* unicast */
+    dest_addr.nl_pid = 0; // For Linux Kernel 
+    dest_addr.nl_groups = 0; // unicast 
 
     nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
     memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
@@ -56,7 +116,7 @@ int main()
     // first time calling the kernel the kernel to give pid
     sendmsg(sock_fd, &msg, 0);
 
-    /* Read message from kernel */
+    // Read message from kernel 
 
     while(1)
     {
@@ -64,7 +124,17 @@ int main()
         printf("USER GOT:%s\n", NLMSG_DATA(nlh));
         if(strcmp(NLMSG_DATA(nlh), "./unload.sh") == 0)
             break;
+        sha_file();
         sendmsg(sock_fd, &msg, 0);
     }    
     close(sock_fd);
+    
 }
+
+
+
+
+
+
+// this is the data from the kernel: NLMSG_DATA(nlh)
+// this is how to change it: strcpy(NLMSG_DATA(nlh), new_string);
